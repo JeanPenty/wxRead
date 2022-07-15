4a36e2c050025f14745361dd3dbf3b60e897b8db
JeanPenty <ceffans1024@gmail.com>
2022-07-15 10:41:09 +0800

    添加工程

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AppNavigator from "./src/navigator/appNavigator";
+function Home() {
+    return (
+        <AppNavigator/>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator  initialRouteName={"Home"}>
+              <Stack.Screen name="Home" component={Home} options={{
+                  headerShown: false,
+              }}/>
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
